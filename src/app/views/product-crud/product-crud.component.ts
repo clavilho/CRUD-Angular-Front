2f57e6efa007d91e2023669573c4d2f0967a730f
Joao Vitor Clavilho <jv.clavilho@hotmail.com>
2021-06-23 10:52:01 -0300

    Att titulos

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { HeaderService } from "./../../components/template/header/header.service";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-crud',
-  templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+  selector: "app-product-crud",
+  templateUrl: "./product-crud.component.html",
+  styleUrls: ["./product-crud.component.css"],
 })
 export class ProductCrudComponent implements OnInit {
-
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: "Cadastrar Produtos",
+      icon: "storefront",
+      dataUr: "/products",
+    };
   }
+
+  ngOnInit(): void {}
   navigateToProductCreate(): void {
-    this.router.navigate(['/products/create'])
+    this.router.navigate(["/products/create"]);
   }
 }
